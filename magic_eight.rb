c1781b82b0996563e8a9243ead41780923303632
Faith Oladele <faitholadele@gmail.com>
2016-05-13 07:48:19 -0600

    updating magic 8 ball

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,28 +1,38 @@
 # User inputs Question
 @answer_arr = ["Yes", "No", "Maybe", "Sometimes", "In 10 years"]
+@user_arr = @answer_arr.clone
 
 def menu
 
 	puts "1. Type a question to get an answer, or type QUIT to end the program."
 	puts "2. Enter an answer"
+	puts "3. All answers"
+	puts "4. Reset"
 
 
 	user_input = gets.strip
 
 	if user_input != "QUIT"	
-		if user_input == "1"
-			puts "Enter Question: "
-			quest = gets.strip
-			puts "The Magic Eight Ball says: #{@answer_arr.sample}"
-		elsif user_input == "2"
-			puts "Enter answer of your choice"
-			ans = gets.strip
-			puts "you entered: #{ans}"
-			@answer_arr << ans 
-		else
-			puts "#{user_input} is not a valid option. Enter correct answer"
-			menu
-		end
+		case user_input
+			when "1"
+				puts "Enter Question: "
+				quest = gets.strip
+				puts "The Magic Eight Ball says: #{@answer_arr.sample}"
+			when "2"
+				puts "Enter answer of your choice"
+				ans = gets.strip
+				if @user_arr.include?(ans) == false
+					puts "you entered: #{ans}"
+					@user_arr << ans 
+				end
+			when "3"
+				@user_arr.each {|all_ans| puts all_ans}
+			when "4"
+				@user_arr = @answer_arr.clone
+			else
+				puts "#{user_input} is not a valid option. Enter correct answer"
+				menu
+			end
 
 	else
 
